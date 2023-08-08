7153ffdbf6ca15dd906de227b10845ffe4224124
genieu99 <74898231+genieu99@users.noreply.github.com>
2023-08-08 20:46:29 +0900

    [Del] documents 폴더 삭제 (#10)

--- documents/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-import os
-
-class Document(models.Model):
-    attached = models.FileField('첨부 파일', upload_to="")
-
-    def get_filename(self):
-        return os.path.basename(self.attached.name)
