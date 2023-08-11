db69a8fb663a233392c271a87e8ee528cc7eb704
genieu99 <yujin0419@sookmyung.ac.kr>
2023-08-11 14:56:46 +0900

    [Feat] 마감임박글 조회 구현

--- posts/urls.py
+++ posts/urls.py
@@ -8,6 +8,7 @@ app_name = "posts"
 urlpatterns = [
     path('programs/', get_programs, name="programs"),
     path('popular/', get_popular, name="popular"),
+    path('imminent/', get_imminent, name="imminent"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
