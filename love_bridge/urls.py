11902f38907af7b2674446da550f830754d4ec19
genieu99 <yujin0419@sookmyung.ac.kr>
2023-08-11 12:40:20 +0900

    [Chore] 모델 필드 이름 변경

--- love_bridge/urls.py
+++ love_bridge/urls.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
-from django.conf import settings
-from django.conf.urls.static import static
+<<<<<<< Updated upstream
+=======
 from django.urls import path, include
-
+from accounts import views
 from mypage import views
+from django.conf.urls.static import static
+>>>>>>> Stashed changes
+from django.conf import settings
+
+<<<<<<< Updated upstream
+=======
+from mypage import views
+>>>>>>> Stashed changes
 from accounts import views as accounts_views
 from posts.views import *
 
@@ -11,10 +19,8 @@ from posts.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', accounts_views.home),
-
     path('accounts/', include('accounts.urls', namespace='accounts')),
     path('posts/', include('posts.urls', namespace='home')),
-    path('', views.home),
     path('mypage/', include('mypage.urls')),
 ]
 
