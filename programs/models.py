bae3f4afc5ec3a7b7a5b89cb7a96b67810c0ad05
genieu99 <yujin0419@sookmyung.ac.kr>
2023-08-19 04:46:51 +0900

    [Chore] 최종 ver.3

--- programs/models.py
+++ programs/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from accounts.models import *
 
 
 class Program(models.Model):
@@ -8,8 +9,8 @@ class Program(models.Model):
     district = models.CharField(verbose_name="지역", max_length=11)
     agency = models.CharField(verbose_name="기관", max_length=15)
     deadline_yy = models.IntegerField(verbose_name="마감일 연")
-    deadline_mm = models.IntegerField(verbose_name="마감일 월")
-    deadline_dd = models.IntegerField(verbose_name="마감일 일")
+    deadline_mm = models.CharField(verbose_name="마감일 월", max_length=2)
+    deadline_dd = models.CharField(verbose_name="마감일 일", max_length=2)
     phone = models.CharField(verbose_name="문의처", max_length=13)
     like = models.IntegerField(verbose_name="좋아요 개수", default=0)
     iflike = models.BooleanField(verbose_name="좋아요 여부", default=False)
@@ -19,19 +20,3 @@ class Program(models.Model):
 
     def __str__(self):
         return str(self.title)
-
-
-class MyProgram(models.Model):
-    program = models.ForeignKey(Program, on_delete=models.CASCADE)
-    # process = models.CharField(verbose_name="진행 상태", max_length=6)
-    process = '서류전달'
-    
-    def str(self):
-        return f'{self.program} - {self.process}'
-
-
-class MyLike(models.Model):
-    program = models.ForeignKey(Program, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return str(self.program)
